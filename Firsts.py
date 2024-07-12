8e5fd0f3916708bec170d2701f15e74b31e3b9c4
Juancho1s <valenzuela.juan.isw@unipolidgo.edu.mx>
2024-07-12 13:03:20 -0500

    Test commit

---
+++ Firsts.py
@@ -0,0 +1,14 @@
+firsts = {
+    "PROGRAM": ["!int"],
+    "BODY": ["var", "{"],
+    "DECLARE": ["var"],
+    "IDENTIFIERS": ["identifiers"],
+    "TYPES": ["integer", "decimal", "string", "bool"],
+    "DEFAULT": ["integer", "decimal", "string", "bool"],
+    "MAIN": ["{"],
+    "STATEMENT": ["identifier"],
+    "EXPRESION": ["+", "-", "||", "{", "identifier", "true", "string", "number"],
+    "EXI": ["+", "-", "||", "{", "identifier", "true", "string", "number"],
+    "EXP": ["+", "-", "||", "{", "identifier", "true", "string", "number"],
+    "REL": ["=", "<", ">", "<=", ">=", "!="]
+}
