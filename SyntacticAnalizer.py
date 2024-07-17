334d1d0ed339df8f8f588edae8dbd84df4ef2f4c
Juan Valenzuela <113451623+Juancho1s@users.noreply.github.com>
2024-07-16 20:38:03 -0600

    predictive matrix correction

--- SyntacticAnalizer.py
+++ SyntacticAnalizer.py
@@ -18,10 +18,6 @@ class LL1Parser:
             top = stack.pop()
             current_token = tokens[index]
 
-            # print(f"Stack: {stack}")
-            # print(f"Current Token: {current_token}")
-            # print(f"Remaining Tokens: {tokens[index:]}")
-
             if top == current_token:
                 index += 1
             elif top in self.grammar and current_token in self.parsing_table[top]:
@@ -36,69 +32,6 @@ class LL1Parser:
         return index == len(tokens)
 
 
-def tokenize(program):
-    keywords = {
-        "!init",
-        "¡end",
-        "var",
-        "integer",
-        "decimal",
-        "string",
-        "bool",
-        "for",
-        "while",
-        "read",
-        "write",
-        "if",
-        "then",
-        "endif",
-        "else",
-        "do",
-    }
-    symbols = {
-        ";",
-        ":",
-        "{",
-        "}",
-        "=",
-        "+",
-        "-",
-        "*",
-        "/",
-        "&&",
-        "||",
-        "<",
-        "<=",
-        "=",
-        ">",
-        ">=",
-        "!=",
-        "(",
-        ")",
-        ":=",
-        ",",
-    }
-    tokens = []
-    token = ""
-    for char in program:
-        if char.isspace():
-            if token:
-                tokens.append(token)
-                token = ""
-        elif char in symbols:
-            if token:
-                tokens.append(token)
-                token = ""
-            tokens.append(char)
-        else:
-            if char != "Â":  # This handles the special character issue
-                token += char
-    if token:
-        tokens.append(token)
-
-    return tokens
-
-
 
 if __name__ == "__main__":
     myInstance = LexicalAnalyzer(
@@ -106,11 +39,13 @@ if __name__ == "__main__":
         "C:\\Users\\POERT\\Desktop\\Universidad\\8th cuater\\compilers and interpreters\\TestFiles\\Errors.txt"
     )
     
-    content = myInstance.getFileContent("C:\\Users\\POERT\\Desktop\\Universidad\\8th cuater\\compilers and interpreters\\Program1.txt")
+    content = myInstance.getFileContent("C:\\Users\\POERT\\Desktop\\Universidad\\8th cuater\\compilers and interpreters\\Program2.txt")
     
     parser = LL1Parser(predictiveMatrix, grammar, "PROGRAM")
 
     myTokens = myInstance.generateToken(content)
+    
+    print(myTokens)
     
     result = parser.parse(myTokens)
     
